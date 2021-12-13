873f4d97d8ac34144fe8aa6dc6c9012062a6848d
Jorge Caballero Mendez <jorge.caballero.mendez@gds.ey.com>
2021-12-13 19:53:31 -0300

    try-catch for branch protection rule

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // GraphQL query to set the branch protection rules
 const addBranchProtection = `
-mutation($repository: ID!, $pattern: String!, $requiresApprovingReviews: Boolean, $requiredApprovingReviewCount: Int, $isAdminEnforced: Boolean, $restrictsPushes: Boolean, $requiresLinearHistory: Boolean, $allowsForcePushes: Boolean, $allowsDeletions: Boolean) {
+mutation($repository: ID!, $pattern: String!, $requiresApprovingReviews: Boolean, $requiredApprovingReviewCount: Int, $isAdminEnforced: Boolean, $restrictsPushes: Boolean, $requiresLinearHistory: Boolean, $allowsForcePushes: Boolean, $allowsDeletions: Boolean, $requiresCommitSignatures: Boolean, $requiresCodeOwnerReviews: Boolean) {
   createBranchProtectionRule(input: {
     repositoryId: $repository,
     pattern:$pattern,
@@ -15,7 +15,9 @@ mutation($repository: ID!, $pattern: String!, $requiresApprovingReviews: Boolean
     restrictsPushes:$restrictsPushes,
     requiresLinearHistory:$requiresLinearHistory,
     allowsForcePushes:$allowsForcePushes,
-    allowsDeletions:$allowsDeletions
+    allowsDeletions:$allowsDeletions,
+    requiresCommitSignatures: $requiresCommitSignatures,
+    requiresCodeOwnerReviews: $requiresCodeOwnerReviews
   })
   {
     branchProtectionRule {
@@ -44,6 +46,8 @@ query($organization: String!, $repo: String!, $cursor: String) {
           requiresLinearHistory
           allowsForcePushes
           allowsDeletions
+          requiresCommitSignatures
+          requiresCodeOwnerReviews
         }
         pageInfo {
           endCursor
@@ -53,7 +57,7 @@ query($organization: String!, $repo: String!, $cursor: String) {
     }
   }
 }
-`
+`;
 
 // GraphQL query to get the template repository name
 const getTemplateRepositoryName = `
@@ -69,83 +73,88 @@ query($organization: String!, $repo: String!) {
     }
   }
 }
-`
-
+`;
 
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+	// Your code here
+	app.log.info("Yay, the app was loaded!");
 
-  app.log.info("loading template repositories patterns");
+	app.log.info("loading template repositories patterns");
 
-  app.on("repository.created", async (context) => {
-    // Get the template repository name and owner
-    const templateRepositoryName = await context.octokit.graphql(getTemplateRepositoryName, {
-      organization: context.payload.organization.login,
-      repo: context.payload.repository.name
-    });
-    // if the template repository name is not null
-    if (templateRepositoryName.organization.repository.templateRepository) {
-    // Get the branch protection rules for the template repository in a for loop
-      let rules = [];
-      let hasNextPage = true;
-      let cursor = null;
-      while (hasNextPage) {
-        const response = await context.octokit.graphql(getBranchProtectionRules, {
-          organization: templateRepositoryName.organization.repository.templateRepository.owner.login,
-          repo: templateRepositoryName.organization.repository.templateRepository.name,
-          cursor: cursor
-        });
-        rules = rules.concat(response.organization.repository.branchProtectionRules.nodes);
-        cursor = response.organization.repository.branchProtectionRules.pageInfo.endCursor;
-        hasNextPage = response.organization.repository.branchProtectionRules.pageInfo.hasNextPage;
-      }
+	app.on("repository.created", async (context) => {
+		// Get the template repository name and owner
+		const templateRepositoryName = await context.octokit.graphql(getTemplateRepositoryName, {
+			organization: context.payload.organization.login,
+			repo: context.payload.repository.name,
+		});
+		// if the template repository name is not null
+		if (templateRepositoryName.organization.repository.templateRepository) {
+			// Get the branch protection rules for the template repository in a for loop
+			let rules = [];
+			let hasNextPage = true;
+			let cursor = null;
+			while (hasNextPage) {
+				const response = await context.octokit.graphql(getBranchProtectionRules, {
+					organization: templateRepositoryName.organization.repository.templateRepository.owner.login,
+					repo: templateRepositoryName.organization.repository.templateRepository.name,
+					cursor: cursor,
+				});
+				rules = rules.concat(response.organization.repository.branchProtectionRules.nodes);
+				cursor = response.organization.repository.branchProtectionRules.pageInfo.endCursor;
+				hasNextPage = response.organization.repository.branchProtectionRules.pageInfo.hasNextPage;
+			}
 
-      // add the branch protection rules from the result
-      for (let i = 0; i < rules.length; i++) {
-        const { data, errors } = await context.octokit.graphql(addBranchProtection, {
-          repository: context.payload.repository.node_id,
-          pattern: rules[i].pattern,
-          requiresApprovingReviews: rules[i].requiresApprovingReviews,
-          requiredApprovingReviewCount: rules[i].requiredApprovingReviewCount,
-          isAdminEnforced: rules[i].isAdminEnforced,
-          restrictsPushes: rules[i].restrictsPushes,
-          requiresLinearHistory: rules[i].requiresLinearHistory,
-          allowsForcePushes: rules[i].allowsForcePushes,
-          allowsDeletions: rules[i].allowsDeletions
-        });
+			// add the branch protection rules from the result
+			for (let i = 0; i < rules.length; i++) {
+        try {
+          const branchProtectionRuleResponse = await context.octokit.graphql(addBranchProtection, {
+            repository: context.payload.repository.node_id,
+            pattern: rules[i].pattern,
+            requiresApprovingReviews: rules[i].requiresApprovingReviews,
+            requiredApprovingReviewCount: rules[i].requiredApprovingReviewCount,
+            isAdminEnforced: rules[i].isAdminEnforced,
+            restrictsPushes: rules[i].restrictsPushes,
+            requiresLinearHistory: rules[i].requiresLinearHistory,
+            allowsForcePushes: rules[i].allowsForcePushes,
+            allowsDeletions: rules[i].allowsDeletions,
+            requiresCommitSignatures: rules[i].requiresCommitSignatures,
+            requiresCodeOwnerReviews: rules[i].requiresCodeOwnerReviews
+          });
 
-        if (errors) {
-          // updates existing branch protection rules
-          throw errors;
+          console.log(`Branch rule added. Pattern ${rules[i].pattern}`);
+          console.log(branchProtectionRuleResponse);
+        } catch (error) {
+          console.log(error);
         }
-      }
+			}
 
-      // retrieve teams from the template repository
-      const templateRepositoryTeams = await context.octokit.request("GET /repos/{org}/{repo}/teams", {
-        org: templateRepositoryName.organization.repository.templateRepository.owner.login,
-        repo: templateRepositoryName.organization.repository.templateRepository.name
-      });
+			// retrieve teams from the template repository
+			const templateRepositoryTeams = await context.octokit.request("GET /repos/{org}/{repo}/teams", {
+				org: templateRepositoryName.organization.repository.templateRepository.owner.login,
+				repo: templateRepositoryName.organization.repository.templateRepository.name,
+			});
 
-      // add teams to repository
-      for (let j = 0; j < templateRepositoryTeams.data.length; j++) {
-        const response = await context.octokit.request("PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}", {
-          org: context.payload.organization.login,
-          team_slug: templateRepositoryTeams.data[j].slug,
-          owner: context.payload.repository.owner.login,
-          repo: context.payload.repository.name,
-          permission: templateRepositoryTeams.data[j].permission
-        });
-      }
+			// add teams to repository
+			for (let j = 0; j < templateRepositoryTeams.data.length; j++) {
+				const { status, url } = await context.octokit.request("PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}", {
+					org: context.payload.organization.login,
+					team_slug: templateRepositoryTeams.data[j].slug,
+					owner: context.payload.repository.owner.login,
+					repo: context.payload.repository.name,
+					permission: templateRepositoryTeams.data[j].permission,
+				});
 
-    } else {
-      app.log.info("no template repository name found for " + context.payload.repository.name);
-    }
-  });
+        console.log(`Team added: ${templateRepositoryTeams.data[j].slug}`);
+        console.log(`Status: ${status}. URL: ${url}`);
+			}
+		} else {
+			app.log.info("no template repository name found for " + context.payload.repository.name);
+		}
+	});
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+	// For more information on building apps:
+	// https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+	// To get your app running against GitHub, see:
+	// https://probot.github.io/docs/development/
 };
